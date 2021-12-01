45619c32ad780d1b3c054ca610136e89d1bcab48
Benno Bielmeier <github@bbenno.com>
2021-12-01 17:40:34 +0100

    01: Solve part two

---
+++ 01 Sonar Sweep/main2.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+INPUT_FILE = ARGV[1] || 'input'
+WINDOWS_SIZE = 3
+
+count = File.readlines(INPUT_FILE)
+            .map(&:to_i)
+            .each_cons(WINDOWS_SIZE)
+            .map(&:sum)
+            .each_cons(2)
+            .count { |x, y| x < y }
+
+puts count
