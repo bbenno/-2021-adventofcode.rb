2fd426a59c37174545e0a4f2ddb158729d0d9008
Benno Bielmeier <github@bbenno.com>
2021-12-02 14:17:26 +0100

    02: Solve part two

---
+++ 02 Dive!/main2.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+INPUT_FILE = ARGV[1] || 'input'
+
+position = { horizontal: 0, depth: 0, aim: 0 }
+
+File.readlines(INPUT_FILE).each do |line|
+  command, number = /^(\w+) (\d+)/.match(line.chomp).captures
+  number = number.to_i
+  case command.chomp
+  when /forward/
+    position[:horizontal] += number
+    position[:depth] += number * position[:aim]
+  when /down/
+    position[:aim] += number
+  when /up/
+    position[:aim] -= number
+  else
+    puts "unknown command '#{command}'"
+  end
+end
+
+puts position[:horizontal] * position[:depth]
