c557ef5da60f5b57657114d9faebc998998a2708
Benno Bielmeier <github@bbenno.com>
2021-12-03 07:36:19 +0100

    03: Solve part one

---
+++ 03 Binary Diagnostic/main.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+INPUT_FILE = ARGV[1] || 'input'
+
+bit_ratios = File.readlines(INPUT_FILE).map do |line|
+  line.chomp.split('').map { |bit| bit.to_i * 2 - 1 }
+end.transpose.map(&:sum)
+
+gamma = bit_ratios.map { |ratio| ratio >= 0 ? '1' : '0' }.join.to_i(2)
+epsilon = bit_ratios.map { |ratio| ratio.negative? ? '1' : '0' }.join.to_i(2)
+
+power_consumption = gamma * epsilon
+puts power_consumption
