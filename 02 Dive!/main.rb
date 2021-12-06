f472abc6863cc82c6d172d1bc49e129ee5f8d2f4
Benno Bielmeier <github@bbenno.com>
2021-12-06 14:52:58 +0100

    Refactor solutions for day 2
    - use strip instead of chomp and trim

--- 02 Dive!/main.rb
+++ 02 Dive!/main.rb
@@ -6,9 +6,10 @@ INPUT_FILE = ARGV[0] || 'input'
 position = { horizontal: 0, depth: 0 }
 
 File.readlines(INPUT_FILE).each do |line|
-  command, number = /^(\w+) (\d+)/.match(line.trim).captures
+  command, number = /^(\w+) (\d+)/.match(line.strip).captures
   number = number.to_i
-  case command.chomp
+
+  case command
   when /forward/
     position[:horizontal] += number
   when /down/
--- 02 Dive!/main2.rb
+++ 02 Dive!/main2.rb
@@ -6,9 +6,10 @@ INPUT_FILE = ARGV[0] || 'input'
 position = { horizontal: 0, depth: 0, aim: 0 }
 
 File.readlines(INPUT_FILE).each do |line|
-  command, number = /^(\w+) (\d+)/.match(line.chomp).captures
+  command, number = /^(\w+) (\d+)/.match(line.strip).captures
   number = number.to_i
-  case command.chomp
+
+  case command
   when /forward/
     position[:horizontal] += number
     position[:depth] += number * position[:aim]
