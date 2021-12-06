654d12bbccd71200742735c0aa752e9860736d0c
Benno Bielmeier <github@bbenno.com>
2021-12-06 14:55:20 +0100

    Refactor solutions for day 4
    - rename methods and variables
    - use Infinity instead of generic -1 as "fail" value

--- 04 Giant Squid/main.rb
+++ 04 Giant Squid/main.rb
@@ -4,70 +4,74 @@
 INPUT_FILE = ARGV[0] || 'input'
 
 class Board
-  attr_reader :lookup, :last_move
+  attr_reader :lookup, :last_match
 
   def initialize(board)
     @rows = board
     @columns = board.transpose
 
     @lookup = board.each_with_index.map do |row, row_id|
-      row.each_with_index.map { |value, column_id| [value, [row_id, column_id]] }
+      row.each_with_index
+         .map { |number, column_id| [number, [row_id, column_id]] }
     end.flatten(1).to_h
 
-    @last_move = nil
-    @moves_processed = 0
+    @last_match = nil
+    @numbers_processed = 0
   end
 
-  def move(number)
-    @moves_processed += 1
-    i, j = @lookup[number]
+  def match(number)
+    @numbers_processed += 1
+    row_id, column_id = @lookup[number]
 
-    return unless i && j
+    return false unless row_id && column_id # Number is not on bingo card
 
-    @rows[i].delete(number)
-    @columns[j].delete(number)
+    @rows[row_id].delete(number)
+    @columns[column_id].delete(number)
 
-    @last_move = number
+    @last_match = number
+
+    true
   end
 
-  def play(numbers, timeout = -1)
+  def play(numbers, timeout: Float::INFINITY)
     numbers.each do |number|
-      move(number)
-      return @moves_processed if won?
+      break if @numbers_processed >= timeout
 
-      break if timeout.positive? && @moves_processed >= timeout
+      match(number)
+      return @numbers_processed if bingo?
     end
 
-    -1 # Unable to win
+    Float::INFINITY # Unable to win
   end
 
-  def won?
+  def bingo?
     @rows.any?(&:empty?) || @columns.any?(&:empty?)
   end
 
-  def sum_unmarked
+  def sum_unmatched
     @rows.map(&:sum).sum
   end
 end
 
-moves, *boards = *File.readlines(INPUT_FILE).map(&:strip).slice_before(/^\s*$/)
+numbers, *boards = *File.readlines(INPUT_FILE).map(&:strip).slice_before(/^\s*$/)
 
-# Unwrap moves
-moves = moves.first.split(',').map(&:to_i)
+# numbers is an one-element array due to slicing
+numbers = numbers.first.split(',').map(&:to_i)
 
-shortest_count_moves = moves.count + 1
+shortest_play_duration = Float::INFINITY
 score = nil
 
 boards.map { |b| b.drop(1) } # Dispose leading slice separation (blank line)
-  .map { |b| b.map { |l| l.split.map(&:to_i) } }
-  .each do |board|
+      .map { |b| b.map { |line| line.split.map(&:to_i) } }
+      .each do |board|
   b = Board.new(board)
+  play_duration = b.play(numbers, timeout: shortest_play_duration)
 
-  count_moves = b.play(moves, shortest_count_moves)
+  next unless play_duration.finite?
 
-  if count_moves.positive? && count_moves < shortest_count_moves
-    shortest_count_moves = count_moves
-    score = b.sum_unmarked * b.last_move
+  if play_duration < shortest_play_duration
+    shortest_play_duration = play_duration
+    score = b.sum_unmatched * b.last_match
   end
 end
 
--- 04 Giant Squid/main2.rb
+++ 04 Giant Squid/main2.rb
@@ -4,68 +4,72 @@
 INPUT_FILE = ARGV[0] || 'input'
 
 class Board
-  attr_reader :lookup, :last_move
+  attr_reader :lookup, :last_match
 
   def initialize(board)
     @rows = board
     @columns = board.transpose
 
     @lookup = board.each_with_index.map do |row, row_id|
-      row.each_with_index.map { |value, column_id| [value, [row_id, column_id]] }
+      row.each_with_index
+         .map { |number, column_id| [number, [row_id, column_id]] }
     end.flatten(1).to_h
 
-    @last_move = nil
-    @moves_processed = 0
+    @last_match = nil
+    @numbers_processed = 0
   end
 
-  def move(number)
-    @moves_processed += 1
-    i, j = @lookup[number]
+  def match(number)
+    @numbers_processed += 1
+    row_id, column_id = @lookup[number]
 
-    return unless i && j
+    return false unless row_id && column_id # Number is not on bingo card
 
-    @rows[i].delete(number)
-    @columns[j].delete(number)
+    @rows[row_id].delete(number)
+    @columns[column_id].delete(number)
 
-    @last_move = number
+    @last_match = number
+
+    true
   end
 
   def play(numbers)
     numbers.each do |number|
-      move(number)
-      return @moves_processed if won?
+      match(number)
+      return @numbers_processed if bingo?
     end
 
-    -1 # Unable to win
+    Float::INFINITY # Unable to win
   end
 
-  def won?
+  def bingo?
     @rows.any?(&:empty?) || @columns.any?(&:empty?)
   end
 
-  def sum_unmarked
+  def sum_unmatched
     @rows.map(&:sum).sum
   end
 end
 
-moves, *boards = *File.readlines(INPUT_FILE).map(&:strip).slice_before(/^\s*$/)
+numbers, *boards = *File.readlines(INPUT_FILE).map(&:strip).slice_before(/^\s*$/)
 
-# Unwrap moves
-moves = moves.first.split(',').map(&:to_i)
+# numbers is an one-element array due to slicing
+numbers = numbers.first.split(',').map(&:to_i)
 
-most_count_moves = -1
+longest_play_duration = Float::INFINITY
 score = nil
 
 boards.map { |b| b.drop(1) } # Dispose leading slice separation (blank line)
-  .map { |b| b.map { |l| l.split.map(&:to_i) } }
-  .each do |board|
+      .map { |b| b.map { |line| line.split.map(&:to_i) } }
+      .each do |board|
   b = Board.new(board)
+  play_duration = b.play(numbers)
 
-  count_moves = b.play(moves)
+  next unless play_duration.finite?
 
-  if count_moves > most_count_moves
-    most_count_moves = count_moves
-    score = b.sum_unmarked * b.last_move
+  if play_duration > longest_play_duration || longest_play_duration.infinite?
+    longest_play_duration = play_duration
+    score = b.sum_unmatched * b.last_match
   end
 end
 
