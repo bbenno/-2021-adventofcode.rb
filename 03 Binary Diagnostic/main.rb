926cd2360f605ddcd86c9f7ad1f0d2381f2918cf
Benno Bielmeier <github@bbenno.com>
2021-12-06 14:53:45 +0100

    Refactor solutions for day 3
    - use strip instead of chomp
    - use size instead of count (better performance)
    - rename function

--- 03 Binary Diagnostic/main.rb
+++ 03 Binary Diagnostic/main.rb
@@ -4,11 +4,11 @@
 INPUT_FILE = ARGV[0] || 'input'
 
 bit_ratios = File.readlines(INPUT_FILE).map do |line|
-  line.chomp.split('').map { |bit| bit.to_i * 2 - 1 }
+  line.strip.split('').map { |bit| bit.to_i * 2 - 1 } # {0,1} -> {-1,1}
 end.transpose.map(&:sum)
 
-gamma = bit_ratios.map { |ratio| ratio >= 0 ? '1' : '0' }.join.to_i(2)
-epsilon = bit_ratios.map { |ratio| ratio.negative? ? '1' : '0' }.join.to_i(2)
+gamma = bit_ratios.map { |ratio| ratio >= 0 ? 1 : 0 }.join.to_i(2)
+epsilon = bit_ratios.map { |ratio| ratio.negative? ? 1 : 0 }.join.to_i(2)
 
 power_consumption = gamma * epsilon
 puts power_consumption
--- 03 Binary Diagnostic/main2.rb
+++ 03 Binary Diagnostic/main2.rb
@@ -4,21 +4,21 @@
 INPUT_FILE = ARGV[0] || 'input'
 
 list = File.readlines(INPUT_FILE).map do |line|
-  line.chomp.split('')
+  line.strip.split('')
 end
 
-def f(l, cmp, i = 0)
-  return l.first if l.count <= 1
+def bit_find(l, cmp, i = 0)
+  return l.first if l.size <= 1
   return nil unless i < l.first.size
 
   ratio = l.map { |bits| bits[i] }.map { |bit| bit.to_i * 2 - 1 }.sum
   common_value = cmp.call(ratio) ? '1' : '0'
 
-  f(l.select { |nr| nr[i] == common_value }, cmp, i + 1)
+  bit_find(l.select { |nr| nr[i] == common_value }, cmp, i + 1)
 end
 
-oxygen_generator_ratio = f(list, ->(ratio) { ratio >= 0 }).join.to_i(2)
-co2_scrubber_ratio = f(list, ->(ratio) { ratio.negative? }).join.to_i(2)
+oxygen_generator_ratio = bit_find(list, ->(ratio) { ratio >= 0 }).join.to_i(2)
+co2_scrubber_ratio = bit_find(list, ->(ratio) { ratio.negative? }).join.to_i(2)
 
 life_support_rating = oxygen_generator_ratio * co2_scrubber_ratio
 puts life_support_rating
