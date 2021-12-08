b5e0b2cc89eea6efdf04fa7b567aca3c04329321
Benno Bielmeier <github@bbenno.com>
2021-12-09 00:00:52 +0100

    08: Tidy up part two for better readability

--- 08 Seven Segment Search/main.rb
+++ 08 Seven Segment Search/main.rb
@@ -4,7 +4,7 @@
 INPUT_FILE = ARGV[0] || 'input'
 
 count = File.readlines(INPUT_FILE).map do |line|
-  line.split('|').last.strip.split.count { |w| [2,3,4,7].include?(w.size) }
+  line.split('|').last.strip.split.count { |w| [2, 3, 4, 7].include?(w.size) }
 end.sum
 
 puts count
--- 08 Seven Segment Search/main2.rb
+++ 08 Seven Segment Search/main2.rb
@@ -3,33 +3,31 @@
 
 INPUT_FILE = ARGV[0] || 'input'
 
-sum = File.readlines(INPUT_FILE) # rubocop:disable Metrics/BlockLength
-          .map do |line|
+sum = File.readlines(INPUT_FILE).map do |line|
   input, output = line.split('|').map do |part|
-    part.strip.split.map { |v| v.split('') }
+    part.strip.split.map { |v| v.split('').map(&:to_sym) }
   end
+
   input.sort_by!(&:size)
+  one_segments = input[0]
+  four_segments = input[2]
 
   output.map do |w|
     case w.size
-    when 2, 3, 4, 7
-      { 2 => 1, 7 => 8, 3 => 7, 4 => 4 }[w.size]
+    when 2 then 1
+    when 3 then 7
+    when 4 then 4
     when 5
-      if (input[0] - w).empty?
-        3
-      elsif (w - input[2]).size == 2
-        5
-      else
-        2
+      if (one_segments - w).empty? then 3
+      elsif (w - four_segments).size == 2 then 5
+      else 2 # rubocop:disable Lint/ElseLayout
       end
     when 6
-      if !(input[0] - w).empty?
-        6
-      elsif (input[2] - w).empty?
-        9
-      else
-        0
+      if (four_segments - w).empty? then 9
+      elsif (one_segments - w).empty? then 0
+      else 6 # rubocop:disable Lint/ElseLayout
       end
+    when 7 then 8
     end
   end.join.to_i
 end.sum
