64fc09124acec7679b4c5fc267222f0f0654963e
Benno Bielmeier <github@bbenno.com>
2021-12-14 10:15:41 +0100

    14: Solve part one

--- 14 Extended Polymerization/README.txt
+++ 14 Extended Polymerization/README.txt
@@ -76,3 +76,5 @@ Apply 10 steps of pair insertion to the polymer template and find the
 most and least common elements in the result. What do you get if you
 take the quantity of the most common element and subtract the quantity
 of the least common element?
+
+Your puzzle answer was 3095.
---
+++ 14 Extended Polymerization/main.rb
@@ -0,0 +1,36 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+INPUT_FILE = ARGV[0] || 'input'
+STEPS = 10
+
+polymer_template = ''
+pair_insertion_rules = []
+
+File.open(INPUT_FILE) do |file|
+  polymer_template = file.readline.strip
+  file.readline # Ignore blank line
+  pair_insertion_rules = file.readlines.map do |line|
+    pair, element = line.split('->').map(&:strip)
+    [pair, [pair[0] + element, element + pair[1]]]
+  end
+end
+
+pair_quantity = polymer_template.chars.each_cons(2).map(&:join).tally
+pair_quantity.default = 0
+
+STEPS.times do
+  next_pair_quantity = pair_quantity.dup
+  pair_insertion_rules.each do |pair, next_pairs|
+    next_pair_quantity[pair] -= pair_quantity[pair]
+    next_pair_quantity[next_pairs[0]] += pair_quantity[pair]
+    next_pair_quantity[next_pairs[1]] += pair_quantity[pair]
+  end
+  pair_quantity = next_pair_quantity
+end
+
+element_quantity = Hash.new(0)
+pair_quantity.each { |pair, count| element_quantity[pair[1]] += count }
+element_quantity[polymer_template[0]] += 1
+
+puts element_quantity.values.max - element_quantity.values.min
